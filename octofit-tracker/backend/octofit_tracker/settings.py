db88e8e7df61568de6448730d64618a68133e814
danielpuertaortiz <daniel.puertaortiz@slalom.com>
2025-04-08 00:23:47 +0000

    Add OctoFit Tracker application story and workshop details

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-u(2z+-#sokm4o1zs5#4e8sgm^$_(o%82+a8k3_e7@w$0d8stry
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'legendary-chainsaw-jj994xpw4wj7c9p5-8000.app.github.dev']
 
 
 # Application definition
